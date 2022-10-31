87c9415b7db594849cefe8c02aab6df155ee6d8a
AcapXasyraF <acapxasyrafx@yahoo.com>
2022-10-31 14:48:41 +0800

    New INTERV-36 Bug Fixed

--- app/Http/Controllers/WorkAnalysisFormController.php
+++ app/Http/Controllers/WorkAnalysisFormController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\WorkAnalysisForm; 
+use App\Models\WorkAnalysisForm;
 use App\Models\JobDescription;
 use App\Models\JobSpecification;
 use App\Models\WorkAnalysisJobSpecification;
@@ -24,7 +24,7 @@ class WorkAnalysisFormController extends Controller
             'company_address2' => '',
             'company_address3' => '',
             'state_id' => 'required|integer',
-            'city_id' => 'required|integer',
+            'city_id' => 'required|string',
             'postcode_id' => 'required|integer',
 
             'supervisor_name' => 'required|string',
@@ -50,7 +50,7 @@ class WorkAnalysisFormController extends Controller
             'job_experience_year' => 'required|string',
             'job_experience_months' => 'required|string',
             'others' => 'required|string',
-            
+
              'location_services' => 'required',
              'services_id' => '',
              'code_id' => '',
@@ -100,7 +100,7 @@ class WorkAnalysisFormController extends Controller
             'job_experience_year' => $request->job_experience_year,
             'job_experience_months' => $request->job_experience_months,
             'others' => $request->others,
-           
+
             'location_services' => $request->location_services,
             'type_diagnosis_id' => $request->type_diagnosis_id,
             'category_services' => $request->category_services,
@@ -111,7 +111,7 @@ class WorkAnalysisFormController extends Controller
         ];
 
         $validateWorkAnalysisForm = [];
- 
+
         if ($request->category_services == 'assisstance' || $request->category_services == 'external') {
             $validateWorkAnalysisForm['services_id'] = 'required';
             $WorkAnalysisForm['services_id'] =  $request->services_id;
@@ -148,27 +148,27 @@ class WorkAnalysisFormController extends Controller
         if ($validator->fails()) {
             return response()->json(["message" => $validator->errors(), "code" => 422]);
         }
-        
+
         // dd($WorkAnalysisForm);
         $ab=WorkAnalysisForm::firstOrCreate($WorkAnalysisForm);
-        $WorkAnalysisFormid=($ab->id);  
+        $WorkAnalysisFormid=($ab->id);
 
         if(!empty($request->jobs)){
             foreach($request->jobs as $key) {
             if($key['task_description']){
              $data = array('task_description' => $key['task_description'],'patient_id' =>$request->patient_id,'objectives'=>$key['objectives'],'procedure'=>$key['procedure'],'rate_of_time'=>$key['rate_of_time'],'work_analysis_form_id'=>$WorkAnalysisFormid);
-                JobDescription::insert($data); 
+                JobDescription::insert($data);
             }
             }
-         } 
+         }
          if(!empty($request->job_specification)){
             foreach($request->job_specification as $key) {
                 if($key['questions']){
              $data = array('question_name' => $key['questions'],'patient_id' =>$request->patient_id,'answer'=>$key['answer'],'comment'=>$key['comments'],'work_analysis_form_id'=>$WorkAnalysisFormid);
-                WorkAnalysisJobSpecification::insert($data); 
+                WorkAnalysisJobSpecification::insert($data);
                 }
             }
-         } 
+         }
 
         return response()->json(["message" => "Work Analysis Form Created Successfully!", "code" => 200]);
 
