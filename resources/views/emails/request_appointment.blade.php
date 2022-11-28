f6cf6433c144a004bfbdf54c1e1ff7d63ce4f6c7
f41zf4u21 <f41zf4u21@gmail.com>
2022-11-28 16:30:44 +0800

    CD 86, 87 Bugs - Fixed

---
+++ resources/views/emails/request_appointment.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>MENTARI MALAYSIA: Appointment Request</title>
+</head>
+
+<body>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden sm:rounded-lg">
+
+                <div class="min-h-screen flex justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+                    <div class="max-w-md w-full space-y-8">
+                        Dear {{ $data['name']}},
+                        <br>
+                        <br>
+                        Your appointment with {{ $data['branch']}} has been confirmed and scheduled
+                        on {{$data['date']}} at {{$data['time']}}.
+                        <br>
+                        <br>
+                        Thank you.
+                        <br>
+                        <img src="{{asset('storage/img/mentari.png')}}" alt="Mentari" title="" style="width:20px; height:auto;">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
