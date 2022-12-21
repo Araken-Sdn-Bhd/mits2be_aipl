a8ceed7474be2cf1974ddb428564c34ffa72b7e0
f41zf4u21 <f41zf4u21@gmail.com>
2022-12-21 10:19:09 +0800

    fixed on se progress note

--- app/Http/Controllers/SeProgressNoteController.php
+++ app/Http/Controllers/SeProgressNoteController.php
@@ -407,6 +407,40 @@ class SeProgressNoteController extends Controller
                         return response()->json(["message" => $e->getMessage(), 'EtpProgress' => $SeProgress, "code" => 200]);
                     }
                     return response()->json(["message" => "Se Progress Note Successfully", "code" => 200]);
+                } else {
+
+                    $SeProgress = [
+                        'services_id' =>  $request->services_id,
+                        'code_id' =>  $request->code_id,
+                        'sub_code_id' =>  $request->sub_code_id,
+                        'added_by' =>  $request->added_by,
+                        'patient_mrn_id' =>  $request->patient_mrn_id,
+                        'patient_id' =>  $request->patient_id,
+                        'name' =>  $request->name,
+                        'mrn' =>  $request->mrn,
+                        'date' =>  $request->date,
+                        'time' =>  $request->time,
+                        'staff_name' =>  $request->staff_name,
+                        'activity_type' =>  $request->activity_type,
+                        'progress_note' =>  $request->progress_note,
+                        'management_plan' =>  $request->management_plan,
+                        'location_service' =>  $request->location_service,
+                        'diagnosis_type' =>  $request->diagnosis_type,
+                        'complexity_service' =>  $request->complexity_service,
+                        'outcome' =>  $request->outcome,
+                        'medication' =>  $request->medication,
+                        'status' => "0",
+                        'appointment_details_id' => $request->appId,
+                    ];
+
+                    try {
+                        $HOD = SeProgressNote::where(
+                            ['id' => $request->id]
+                        )->update($SeProgress);
+                    } catch (Exception $e) {
+                        return response()->json(["message" => $e->getMessage(), 'SEProgress' => $SeProgress, "code" => 200]);
+                    }
+                    return response()->json(["message" => "Se Progress Note Successfully", "code" => 200]);
                 }
             } else {
                 if ($request->service_category == 'assisstance' || $request->service_category == 'external') {
@@ -536,7 +570,7 @@ class SeProgressNoteController extends Controller
                     try {
                         $HOD = SeProgressNote::create($SeProgress);
                     } catch (Exception $e) {
-                        return response()->json(["message" => $e->getMessage(), 'EtpProgress' => $SeProgress, "code" => 200]);
+                        return response()->json(["message" => $e->getMessage(), 'SEProgress' => $SeProgress, "code" => 200]);
                     }
                     return response()->json(["message" => "Se Progress Note Successfully", "code" => 200]);
                 }
