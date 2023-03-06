fce4d91740c36c1f847923dbf3d9c1a25e71002d
MOHDHAZIZI <82504629+MOHDHAZIZI@users.noreply.github.com>
2023-03-06 10:52:18 +0800

    faq

--- app/Http/Controllers/FaqListController.php
+++ app/Http/Controllers/FaqListController.php
@@ -19,14 +19,65 @@ class FaqListController extends Controller
         return response()->json(["message" => "List.", 'list' => $list, "code" => 200]);
     }
 
+    public function getSearchList(Request $request)
+    {
+
+        if ($request->keyword == "no-keyword" && $request->category == ""){
+
+        $list = DB::table('faq_list')
+        ->select('question','answer')
+        ->where('isactive',0)
+        ->orderBy('index','ASC')
+        ->get();
+
+        }else if ($request->keyword != "no-keyword" && $request->category != ""){
+
+      
+        $list = DB::table('faq_list')
+        ->select('question','answer')
+        ->where('faq_category_id',$request->category)
+        ->where('isactive',0)
+        ->where(
+            function($query) use ($request) {
+              return $query
+              ->where('question', 'LIKE', '%' . $request->keyword . '%')
+              ->orWhere('answer', 'LIKE', '%' . $request->keyword . '%');
+             })
+       
+        ->orderBy('index','ASC')
+        ->get();
+
+        }else if ($request->keyword != "no-keyword" && $request->category == ""){
+           
+            $list = DB::table('faq_list')
+
+            ->select('question','answer')
+            ->where('isactive',0)
+            ->where('question', 'LIKE', '%' . $request->keyword . '%')
+            ->orWhere('answer', 'LIKE', '%' . $request->keyword . '%')
+            ->orderBy('index','ASC')
+            ->get(); 
+        }
+
+        return response()->json(["message" => "List.", 'list' => $list, "code" => 200]);
+    }
+
     public function getFaqListbyId(Request $request)
     {
+        if($request->category_id != ""){
         $list = DB::table('faq_list')
         ->select('question','answer')
         ->where('isactive',0)
         ->where('faq_category_id',$request->category_id)
         ->orderBy('index','ASC')
         ->get();
+        }else{
+            $list = DB::table('faq_list')
+            ->select('question','answer')
+            ->where('isactive',0)
+            ->orderBy('index','ASC')
+            ->get(); 
+        }
 
         return response()->json(["message" => "List.", 'list' => $list, "code" => 200]);
     }
--- routes/api.php
+++ routes/api.php
@@ -807,6 +807,7 @@ Route::group(['prefix' => 'default-role-access'], function () {
 });
 Route::group(['prefix' => 'faqList'], function () {
     Route::post('/list', [FaqListController::class, 'getFaqList']);
+    Route::post('/search', [FaqListController::class, 'getSearchList']);
     Route::post('/listbyId', [FaqListController::class, 'getFaqListbyId']);
 });
 Route::group(['prefix' => 'faqCategory'], function () {
