9e04eb2051d246ec9cc1884e926c6f88ed5288cb
MOHDHAZIZI <82504629+MOHDHAZIZI@users.noreply.github.com>
2023-04-20 07:51:36 +0800

    job club

--- app/Models/JobClubProgressNote.php
+++ app/Models/JobClubProgressNote.php
@@ -12,5 +12,5 @@ class JobClubProgressNote extends Model
     protected $fillable = ['id', 'name', 'mrn','patient_mrn_id', 'appointment_details_id', 'added_by', 'date', 'time', 'staff_name', 'work_readiness',
     'progress_note', 'management_plan', 'location_service', 'diagnosis_type', 'service_category',
     'services_id', 'code_id', 'sub_code_id', 'complexity_service', 'outcome',
-    'medication', 'status','created_at','add_code_id', 'add_sub_code_id'];
+    'medication', 'status','created_at','add_code_id', 'add_sub_code_id','add_diagnosis_type'];
 }
