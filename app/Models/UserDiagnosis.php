e7de715673a33c49f4e95c1cbda4beb25043f4f3
Khadijah3008 <khadijah3008@gmail.com>
2023-10-10 12:38:22 +0800

    Maintenance MITS2 - Psychiatric Progress Note

--- app/Models/UserDiagnosis.php
+++ app/Models/UserDiagnosis.php
@@ -10,6 +10,6 @@ class UserDiagnosis extends Model
     use HasFactory;
     protected $table = 'user_diagnosis';
     protected $fillable = ['app_id','patient_id','diagnosis_id','add_diagnosis_id','code_id', 'sub_code_id', 'add_code_id', 'add_sub_code_id',
-     'outcome_id','category_services','created_at','updated_at'];
+     'outcome_id','category_services','remarks','created_at','updated_at'];
 
 }
